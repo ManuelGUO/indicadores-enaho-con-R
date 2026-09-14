c2e6437c294a5558749560da6d9261780f6212d0
Very-Cute-Anime-Girl <107881952+Very-Cute-Anime-Girl@users.noreply.github.com>
2026-09-14 13:44:18 -0500

    Lectura y asignación de etiquetas por departamento

--- 1 Indicadores de pobreza.R
+++ 1 Indicadores de pobreza.R
@@ -1,1 +1,21 @@
+# Librerías ----
+library(haven)
+library(dplyr)
+library(janitor)
+library(stringr)
+
+
 # Indicadores de pobreza ----
+
+ruta_data <- "Data/Enaho 2025/"
+
+sumaria <- read_dta(paste0(ruta_data, "1031-Modulo34/sumaria-2025.dta"))
+hogar <- read_dta(paste0(ruta_data, "1031-Modulo01/enaho01-2025-100.dta"))
+
+
+# Variable geográficas ----
+## Departamento ----
+sumaria <- sumaria %>%
+  mutate(
+    ubigeo_departamento = substr(ubigeo, 1, 2)
+  )
